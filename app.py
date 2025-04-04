32b6398dc7404cb652723fdf92cb879b47fdf3cd
Divya-127 <shahdivya127@gmail.com>
2025-04-05 01:04:29 +0530

    specify port

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def generate():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
